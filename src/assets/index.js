faa694774c49d5674d575bc4b0a2d08f236fb7bd
TYT_AKHILP <akhilise.rymec@gmail.com>
2024-03-19 00:46:15 +0530

    project-sec added

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,12 +21,13 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
+
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
 
-import carrent from "./carrent.png";
+import utube from "./utube.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
@@ -56,7 +57,7 @@ export {
   shopify,
   starbucks,
   tesla,
-  carrent,
+  utube,
   jobit,
   tripguide,
 };
